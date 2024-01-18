3c5b5ad732794e3644aea16fde58471f6d7f3f4d
Siddharth Tripathi <tripathisiddharth058@gmail.com>
2024-01-18 22:40:06 +0530

    Quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-function App() {}
+import Header from "./component/Header.jsx";
+import Quiz from "./component/Quiz.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
+}
 
 export default App;
